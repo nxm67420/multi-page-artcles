aedceade1833139ad357b350c9151a53858a5e2d
nxm67420 <46074688+nxm67420@users.noreply.github.com>
2022-03-26 14:30:08 -0500

    Added Query Parameter

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+
 export default function Contact() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name");
+
   return (
     <div>
-        <h2>Contact</h2>
+      <h2>Hello {name}, Contact</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nulla tempore est
         expedita nisi eveniet odio dicta. Tempora expedita sint dicta, ducimus
         necessitatibus, at magni aut porro, quis nam explicabo!</p>  
